020641e57acbf4431d6998bf5e90732df64db1bf
muhmedwagdy <xmohamed.wagdy2016@gmail.com>
2024-01-20 17:08:43 +0300

    add timezone and user auth

--- Course/models.py
+++ Course/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.contrib.auth import User
+
+from django.utils import timezone
+
+
 # Create your models here.
 
 catgorey_name = [('software_test','software_test'),('devoloper','devoloper'),('Data_Analysis','Data_Analysis')]
@@ -10,6 +15,7 @@ catgorey_name = [('software_test','software_test'),('devoloper','devoloper'),('D
 class Course(models.Model):
     name= models.CharField(max_length=200,blank=True, null=True)
     subtitle=models.name = models.CharField(max_length=300,blank=True, null=True)
+    discription = models.TextField(max_length= 1000)
     price = models.FloatField(max_length=10)
     category=models.Choices(catgorey_name)
     
@@ -17,7 +23,7 @@ class Course(models.Model):
 
 
 class reviews(models.Model):
-    name = ''
+    name = models.ForeignKey(Course,on_delete= models.CASCADE, related_name='reviews')
 
-    
+
 
