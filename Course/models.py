169a9e639d2c511105c2a6ce1f1daebcaef9b407
muhmedwagdy <xmohamed.wagdy2016@gmail.com>
2024-01-20 17:40:24 +0300

    add modify class choices

--- Course/models.py
+++ Course/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 from django.utils import timezone
 
 
 # Create your models here.
+FLAG_TYPES = (
+    ('software_test','software_test'),
+    ('devoloper','devoloper'),
+    ('Data_Analysis','Data_Analysis'),
+)
 
-catgorey_name = [('software_test','software_test'),('devoloper','devoloper'),('Data_Analysis','Data_Analysis')]
+
 
 class Course(models.Model):
+    
+
+
     name= models.CharField(max_length=200,blank=True, null=True)
     subtitle=models.name = models.CharField(max_length=300,blank=True, null=True)
     discription = models.TextField(max_length= 1000)
     price = models.FloatField(max_length=10)
-    category=models.Choices(catgorey_name)
+    category=models.CharField(choices= FLAG_TYPES,max_length=20)
 
 
 
     def __str__(self):
         return self.name
     
-
+  
 class Reviews(models.Model):
+    # User maybe comment review  Use SET_NULL
     user = models.ForeignKey(User, related_name='user_review', on_delete=models.SET_NULL, null=True)
-    course = models.ForeignKey(Course, related_name='courses_review', on_delete=models.CASCADE)
-    review = models.TextField(max_length=500)
+    course = models.ForeignKey(Course, related_name='course_review', on_delete=models.CASCADE)
+    review = models.TextField(max_length=400)
     rate = models.IntegerField()
     created_at = models.DateTimeField(default=timezone.now)
     def __str__(self):
