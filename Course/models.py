639efcaa9cdc868f5712a11ec8dd59ce41a9b4b5
muhmedwagdy <xmohamed.wagdy2016@gmail.com>
2024-01-20 16:48:15 +0300

    add install django

---
+++ Course/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Course(models.Model):
+    name=''
+    subtitle=''
+    description=''
+    price =''
+    category=''
+    reviews=''
+
